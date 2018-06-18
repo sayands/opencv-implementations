394153df1d7548d0263244a04dbec2c48e03d05e
sayands <sayandsarkar.1997@gmail.com>
2018-06-18 15:35:27 +0300

    Added RGB descriptor for the search engine

---
+++ ImageSearchEngine/imagesearchutil/rgbhistogram.py
@@ -0,0 +1,17 @@
+# import the necessary packages
+import imutils
+import cv2
+
+class RGBHistogram:
+    def __init__(self, bins):
+        # store the no.of bins the histogram will use
+        self.bins = bins
+
+        def describe(self, image):
+            # compute a 3D histogram in the RGB colorspace
+            # then normalise the image
+            hist = cv2.calcHist([image], [0, 1, 2], None, self.bins, [0, 256, 0, 256, 0, 256])
+            
+            hist = cv2.normalize(hist, hist)
+
+            return hist.flatten()
