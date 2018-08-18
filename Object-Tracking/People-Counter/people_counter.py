6b4c22820e369206b4d6ff2a0ca9003094a19572
sayands <sayandsarkar.1997@gmail.com>
2018-08-18 22:43:43 +0530

    Added People Counting

--- Object-Tracking/People-Counter/people_counter.py
+++ Object-Tracking/People-Counter/people_counter.py
@@ -161,3 +161,77 @@ while True:
     # use the centroid tracker to associate the old object
     # centroids with newly computed object centroids
     objects = ct.update(rects)
+
+    # loop over the tracked objects
+    for (objectID, centroid) in objects.items():
+        # check to see if a trackable object exists for the current
+        # object ID
+        to = trackableObjects.get(objectID, None)
+
+        # otherwise, there is a trackable object so we can utilize 
+        # it to determine direction
+        if to is None:
+            to = TrackableObject(objectID, centroid)
+        
+        else:
+            # difference between y-coordinates of the *current* centroid
+            # and the mean of *previous* centroids will tell us in which
+            # direction the object is moving ( negative for 'up' and 
+            # position for 'down')
+            y = [c[1] for c in to.centroids]
+            direction = centroid[1] - np.mean(y)
+            to.centroids.append(centroid) 
+
+            # check to see if the object has been counted or not
+            if not to.counted:
+                # if the direction is negative(indicating the object is
+                # moving up) AND the centroid is above the center line
+                # Count the object
+                if direction < 0 and centroid[1] < H // 2:
+                    totalUp += 1
+                    to.counted = True
+
+                elif direction > 0 and centroid[1] > H // 2:
+                    totalDown += 1
+                    to.counted = True
+
+        # store the trackable object in our dictionary
+        trackableObjects[objectID] = to
+
+        # draw both the ID of the object and the centroid of the
+        # object on the output frame
+        text = "ID {}".format(objectID)
+        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+    
+
+    # construct a tuple of information we will be displaying on the frame
+    info = [
+        ("Up", totalUp),
+        ("Down", totalDown),
+        ("Status", status),
+    ]
+
+    # loop over the info tuples and draw them on our frame
+    for (i, (k, v)) in enumerate(info):
+        text = "{}: {}".format(k, v)
+        cv2.putText(frame, text, (10, H - ((i * 20) + 20)), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+    
+    # check to see if we should write frame to disk
+    if writer is not None:
+        writer.write(frame)
+    
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the 'q' key was pressed, break from the  loop
+    if key == ord('q'):
+        break
+    
+    # increment the total no.of frames processed thus far and
+    # then update the FPS counter
+    totalFrames += 1
+    fps.update()
