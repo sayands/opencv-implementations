b4086afdcd63b6ebc6f47f18379094d709e1a4cf
sayands <sayandsarkar.1997@gmail.com>
2018-08-21 01:13:38 +0530

    Added script for detecting scores for text in images

--- Text-Detection/text_detection.py
+++ Text-Detection/text_detection.py
@@ -35,3 +35,68 @@ image = cv2.resize(image, (newW, newH))
 layerNames = [
     "feature_fusion/Conv_7/Sigmoid",
     "feature_fusion/concat_3"]
+
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
+net = cv2.dnn.readNet(args["east"])
+
+# construct a blob from the image and then perform a forward pass
+# of the model
+blob = cv2.dnn.blobFromImage(image, 1.0, (W, H), (123.68, 116.78, 103.94), swapRB = True, crop = False)
+start = time.time()
+net.setInput(blob)
+(scores, geometry) = net.forward(layerNames)
+end = time.time()
+
+# show the timing information on text prediction
+print("[INFO] text detection took {:.6f} seconds".format(end - start))
+
+# grab the number of rows and columns from the score volume
+# then initialise our set of bounding box rectangles and
+# corresponding confidence scores
+(numRows, numCols) = scores.shape[2:4]
+rects = []
+confidences = []
+
+# loop over the no.of rows
+for y in numRows:
+    # extract the scores, followed by the geometrical data
+    # used to derive potential bounding box coordinates that
+    # surround text
+    scoresData = scores[0, 0, y]
+    xData0 = geometry[0, 0, y]
+    xData1 = geometry[0, 1, y]
+    xData2 = geometry[0, 2, y]
+    xData3 = geometry[0, 3, y]
+    anglesData = geometry[0, 4, y]
+
+    # loop over the no.of columns
+    for x in range(0, numCols):
+        # if our score does not have sufficient probability, ignore it
+        if scoresData[x] < args["min-confidence"]
+            continue
+        
+        # compute the offset factor as our resulting feature maps will be 4x smaller
+        # than input image
+        (offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+        # extract the rotation angle for the prediction and compute the sine and cosine
+        angle = anglesData[x]
+        cos = np.cos(angle)
+        sin = np.sin(angle)
+
+        # use the geometry volume to derive the width and height of the bounding box
+        h = xData0[x] + xData2[x]
+        w = xData1[x] + xData3[x]
+
+        # compute both the starting and ending (x, y) coordinates for the
+        # text prediction bounding box
+        endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+        endY = int(offsetY + (sin * xData1[x]) + (cos * xData2[x]))
+        startX = int(endX - w)
+        startY = int(endY - h)
+
+        # add the bounding box coordinates and probability score to our 
+        # respective lists
+        rects.append((startX, startY, endX, endY))
+        confidences.append(scoresData[x])
