a631688614b6ecd0fc7eea23a89ddec5c74ef58a
sayands <sayandsarkar.1997@gmail.com>
2018-09-02 01:28:26 +0530

    Code Cleanup

--- Neural-Style-Transfer/neural_style_transfer_image.py
+++ Neural-Style-Transfer/neural_style_transfer_image.py
@@ -20,7 +20,7 @@ image = imutils.resize(image, width = 600)
 (h, w) = image.shape[:2]
 
 # construct a blob from image, set input and perform a forward pass
-blob = cv2.dnn.blobFromImage(image, 1.0, (w. h), (103.969, 116.779, 123.680), swapRB = False, crop = False)
+blob = cv2.dnn.blobFromImage(image, 1.0, (w,  h), (103.969, 116.779, 123.680), swapRB = False, crop = False)
 net.setInput(blob)
 start = time.time()
 output = net.forward()
@@ -30,6 +30,14 @@ end = time.time()
 output = output.reshape((3, output.shape[2], output.shape[3]))
 output[0] += 103.969
 output[1] += 116.779
-output[3] += 123.680
+output[2] += 123.680
 output /= 255.0
 output = output.transpose(1, 2, 0)
+
+# show relevant information and images
+print("[INFO] neural style transfer took {:.4f} seconds".format(end - start))
+
+# show the images
+cv2.imshow("Input", image)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
