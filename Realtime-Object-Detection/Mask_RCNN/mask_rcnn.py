3f14d4694acce82daa5f7f1e2bb0b615788ae8ce
sayands <sayandsarkar.1997@gmail.com>
2018-10-03 00:37:41 +0530

    Added postprocessing and drawing box for showing of mask and detections

--- Realtime-Object-Detection/Mask_RCNN/mask_rcnn.py
+++ Realtime-Object-Detection/Mask_RCNN/mask_rcnn.py
@@ -16,6 +16,70 @@ parser.add_argument('--image', help = 'Path to image file')
 parser.add_argument('--video', help = 'Path to video file')
 args = parser.parse_args()
 
+# Draw the predicted bouding box, colorise and show the mask on image
+def drawBox(frame, classId, conf, left, top, right, bottom, classMask):
+    # Draw a bounding box
+    cv.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
+
+    # Print a label of class
+    label = '%.2f' % conf
+    if classes:
+        assert(classId < len(classes))
+        label = '%s:%s' % (classes[classId], label)
+
+    # Display the label at the top of the bounding box
+    labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+    top = max(top, labelSize[1])
+    cv.rectangle(frame, (left, top - round(1.5 * labelSize[1])), (left + round(1.5 * labelSize[0]), top + baseLine), (255, 255, 255), cv.FILLED)
+    cv.putText(frame, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
+
+    # Resize the mask, threshold, color and apply on image
+    classMask = cv.resize(classMask, (right - left + 1, bottom - top + 1))
+    mask = (classMask > maskThreshold)
+    roi = frame[top:bottom + 1, left:right + 1][mask]
+    color = colors[classId % len(colors)]
+
+    frame[top:bottom + 1, left:right + 1][mask] = ([0.5 * color[0], 0.5 * color[1], 0.5 * color[2]] + 0.5 * roi).astype(np.uint8)
+
+# For each frame, extract the bounding box and mask for each detected object
+def postprocess(boxes, masks):
+    # Output size - N*C*H*W
+    # N - no.of detected boxes
+    # C - no.of classes(excluding background)
+    # H*W - segmentation shape
+    numClasses = masks.shape[1]
+    numDetections = boxes.shape[2]
+
+    frameH = frame.shape[0]
+    frameW = frame.shape[1]
+
+    for i in range(numDetections):
+        box = boxes[0, 0, i]
+        mask = masks[i]
+        score = box[2]
+
+        if score > confThreshold:
+            classId = int(box[1])
+
+            # Extract the bounding box 
+            left = int(frameW * box[3])
+            top = int(frameH * box[4])
+            right = int(frameW * box[5])
+            bottom = int(frameH * box[6])
+
+            left = max(0, min(left, frameW - 1))
+            top = max(0, min(top, frameH - 1))
+            right = max(0, min(right, frameW - 1))
+            bottom = max(0, min(bottom, frameH - 1))
+
+            # Extract the mask for the object
+            classMask = mask[classId]
+
+            # Draw the bounding box, colorize and show the mask on image
+            drawBox(frame, classId, score, left, top, right, bottom, classMask)
+
+
+
 # Loading names of classes
 classesFile = 'model/mscoco_labels.names'
 classes = None 
@@ -33,11 +97,11 @@ for i in range(len(colorsStr)):
     colors.append(color)
 
 # Loading the textGraph nad weight files for the model
-textGraph = "./model/mask_rcnn_inception_v2_coco_2018_01_28.pbtxt"
-modelWeights = "./model/frozen_inference_graph.pb"
+textGraph = "./model/mask_rcnn_inception_v2_coco_2018_01_28.pbtxt";
+modelWeights = "./model/frozen_inference_graph.pb";
 
 # Loading the network
-net = cv2.dnnreadNetFromTensorflow(modelWeights, textGraph)
+net = cv.dnn.readNetFromTensorflow(modelWeights, textGraph);
 net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
