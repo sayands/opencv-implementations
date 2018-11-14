61f686c2a6733e7ebf2178946ece9e493a0277c0
sayands <sayandsarkar.1997@gmail.com>
2018-11-14 19:41:38 +0530

    Added script for Object Detection and writing output video

--- Realtime-Object-Detection/Yolo-v3/yolo_video.py
+++ Realtime-Object-Detection/Yolo-v3/yolo_video.py
@@ -1,51 +1,132 @@
-# Import the neccessary packages
-import numpy as np 
+# import the necessary packages
+import numpy as np
 import argparse
 import imutils
-import time 
+import time
 import cv2
-import os 
+import os
 
-# Construct the argument parser and parse the arguments
+# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", required = True, help = "Path to input video")
-ap.add_argument("-o", "--output", required = True, help = "Path to output video")
-ap.add_argument("-y", "--yolo", required = True, help = "base path to YOLO directory")
-ap.add_argument("-c", "--confidence", type = float, default = 0.5, help = "Minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type = float, default = 0.3, help = "threshold when applying non-max suppression")
+ap.add_argument("-i", "--input", required=True,
+	help="path to input video")
+ap.add_argument("-o", "--output", required=True,
+	help="path to output video")
+ap.add_argument("-y", "--yolo", required=True,
+	help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
 args = vars(ap.parse_args())
 
 # load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
-# initialise a list of colors to represent each possible class label
+# initialize a list of colors to represent each possible class label
 np.random.seed(42)
-COLORS = np.random.randint(0, 255, size = (len(LABELS), 3), dtype = "uint8")
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
 
 # derive the paths to the YOLO weights and model configuration
 weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
 
-# load our YOLO Object detector trained on COCO dataset
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# initialise the video stream, pointer to output video file and frame dimension
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
 vs = cv2.VideoCapture(args["input"])
-writer = None 
-(W, H) = (None , None)
+writer = None
+(W, H) = (None, None)
 
-# try to determine the total no.of frames in the video file 
+# try to determine the total number of frames in the video file
 try:
-    prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.iscv2() else cv2.CV_CAP_PROP_FRAME_COUNT
-    total = int(vs.get(prop))
-    print("[INFO] {} total frames in video".format(total))
+	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+		else cv2.CAP_PROP_FRAME_COUNT
+	total = int(vs.get(prop))
+	print("[INFO] {} total frames in video".format(total))
 
+# an error occurred while trying to determine the total
+# number of frames in the video file
 except:
-    print("[INFO] could not determin # of frames in video")
-    print("[INFO] no approx. completion time can be provided")
-    total -= 1
+	print("[INFO] could not determine # of frames in video")
+	print("[INFO] no approx. completion time can be provided")
+	total = -1
+
+
+while True:
+    (grabbed, frame) = vs.read()
+
+    if not grabbed:
+        break
     
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB = True, crop = False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > args["confidence"]:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+    
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
+
+    # ensure at least one detectione exists
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            # draw a bounding box rectangle and label on the frame
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            cv2.putText(frame, text, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    # check if video writer is None
+    if writer is None:        
+        # intialise the video writer
+        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+        writer = cv2.VideoWriter(args["output"], fourcc, 30, (frame.shape[1], frame.shape[0]), True)
+
+        if total > 0:
+            elap = (end - start)
+            print("[INFO] Single Frame took {:.4f} seconds".format(elap))
+            print("[INFO] Estimated Total Time To Finish: {:.4f}".format(elap * total))
+    
+    # write the output frame to disk
+    writer.write(frame)
+
+# release the file pointers
+print("[INFO] cleaning up...")
+writer.release()
+vs.release()
